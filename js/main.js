5812e6f0c90f3a9580ca9bdebdac134b876b13eb
taxiradio5544 <256445964+taxiradio5544@users.noreply.github.com>
2026-01-24 19:58:43 -0300

    actualización script

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@
 let productos = [];
 let productosBase = [];
 
+//script nuevo
 fetch("https://script.google.com/macros/s/AKfycbwtJU1Npo3-6E4_BYHCX2hNmEejXQks1kvDlbFCXeVhBtSn7hdfiw7eDNJkt33dV-yF/exec")
   .then(r => r.json())
   .then(data => {
